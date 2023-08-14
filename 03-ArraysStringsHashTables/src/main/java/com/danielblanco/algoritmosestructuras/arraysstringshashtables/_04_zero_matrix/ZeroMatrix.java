8231ff2e25f96f77d207c37447fdaa29ab45c79c
diego <diegocarrp@gmail.com>
2023-08-13 20:24:30 -0400

    arrays, string y hash tables

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_04_zero_matrix/ZeroMatrix.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_04_zero_matrix/ZeroMatrix.java
@@ -1,8 +1,5 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._04_zero_matrix;
 
-
-import java.util.Arrays;
-
 /*
  * Dada una matriz, escribe un algoritmo para establecer ceros en la fila F y columna C si existe un
  * 0 en la celda F:C
@@ -17,51 +14,55 @@ import java.util.Arrays;
  *          7 0 1 0 8
  *          0 0 0 0 0
  *          9 0 4 0 9
- *
- * Al recorrer una matriz y llamar a matrix.length obtendremos la cantidad de filas,
- * mientras que al llamar a matrix[n].length obtendremos la cantidad de columnas de la fila
- *
  */
-public class ZeroMatrix {
+public class  ZeroMatrix {
 
   public void zeroMatrix(int[][] matrix) {
+    boolean firstRowHasZero = hasFirstRowAnyZeroes(matrix);
+    boolean firstColHasZero = hasFirstColAnyZeroes(matrix);
 
-    boolean firstRowHasZero = hasFirstRowAnyZero(matrix);
-    boolean firstColHasZero = hasFirstColAnyZero(matrix);
+    // Comprobamos los ceros en el resto de la matriz.
+    checkForZeroes(matrix);
 
-    checkForZeroes( matrix );
-    processRows( matrix );
-    processCols( matrix );
+    processRows(matrix);
+    processColumns(matrix);
 
-    if(firstRowHasZero) {
-      setRowToZero( matrix, 0 );
+    // Si la primera fila o columna tenía un cero, debemos procesarlas también.
+    if (firstRowHasZero) {
+      setRowToZero(matrix, 0);
     }
 
-    if(firstColHasZero) {
-      setColToZero( matrix, 0 );
+    if (firstColHasZero) {
+      setColToZero(matrix, 0);
     }
-
-    System.out.println( Arrays.deepToString( matrix ) );
   }
 
-  private boolean hasFirstRowAnyZero( int[][] matrix ) {
-    for (int row = 0; row < matrix[0].length ;row++) {
-      if(matrix[row][0] == 0) return true;
+  private boolean hasFirstRowAnyZeroes(int[][] matrix) {
+    for (int i = 0; i < matrix[0].length; i++) {
+      if (matrix[0][i] == 0) {
+        return true;
+      }
     }
     return false;
   }
 
-  private boolean hasFirstColAnyZero( int[][] matrix ) {
-    for (int col = 0; col < matrix.length; col++) {
-      if(matrix[0][col] == 0) return true;
+  private boolean hasFirstColAnyZeroes(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      if (matrix[i][0] == 0) {
+        return true;
+      }
     }
     return false;
   }
 
+  /*
+   * Si encontramos un cero en F:C, escribimos un cero en la fila F de la primera columna y en la
+   * columna C de la primera fila. Así marcamos que la fila y la columna debe ser procesada.
+   */
   private void checkForZeroes(int[][] matrix) {
     for (int row = 1; row < matrix.length; row++) {
-      for(int col = 1; col < matrix[0].length; col++) {
-        if(matrix[row][col] == 0) {
+      for (int col = 1; col < matrix[0].length; col++) {
+        if (matrix[row][col] == 0) {
           matrix[row][0] = 0;
           matrix[0][col] = 0;
         }
@@ -71,30 +72,28 @@ public class ZeroMatrix {
 
   private void processRows(int[][] matrix) {
     for (int row = 0; row < matrix.length; row++) {
-      if(matrix[row][0] == 0) {
+      if (matrix[row][0] == 0) {
         setRowToZero(matrix, row);
       }
     }
   }
 
-  private void setRowToZero( int[][] matrix,
-                             int row ) {
-    for(int col = 0; col < matrix[0].length; col++) {
+  private void setRowToZero(int[][] matrix, int row) {
+    for (int col = 0; col < matrix[0].length; col++) {
       matrix[row][col] = 0;
     }
   }
 
-  private void processCols(int[][] matrix) {
+  private void processColumns(int[][] matrix) {
     for (int col = 0; col < matrix[0].length; col++) {
-      if(matrix[0][col] == 0) {
+      if (matrix[0][col] == 0) {
         setColToZero(matrix, col);
       }
     }
   }
 
-  private void setColToZero( int[][] matrix,
-                             int col ) {
-    for(int row = 0; row < matrix.length; row++) {
+  private void setColToZero(int[][] matrix, int col) {
+    for (int row = 0; row < matrix.length; row++) {
       matrix[row][col] = 0;
     }
   }
